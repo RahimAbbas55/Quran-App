1cb5f78a03f16eeb756369b2e138a0f8b4ae3831
RahimAbbas55 <rahim.abbas27@gmail.com>
2025-05-17 16:27:55 +0500

    Addes new type params to the /Types/types.ts

--- Types/types.ts
+++ Types/types.ts
@@ -4,3 +4,13 @@ export type AuthStackParamList = {
     SignUp: undefined;
     ForgotPassword: undefined;
 }
+
+export type MainStackParamList = {
+    Homepage: undefined;
+    LearningPath: undefined;
+    Memorization: undefined;
+    QuranReading: undefined;
+    Settings: undefined;
+    Tafsir: undefined;
+    AIBot: undefined;
+}
